c14b2e9a8f561d119386c439b1dafa944a00e7ef
vishwas-tech29 <rallapallivishwas76@gmail.com>
2026-01-29 16:16:10 +0530

    trhgs

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com', 'via.placeholder.com'],
   },
+  webpack: (config, { isServer }) => {
+    // Exclude backend folder from compilation
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: ['**/backend/**', '**/node_modules/**']
+    }
+    
+    return config
+  },
 }
 
 module.exports = nextConfig
